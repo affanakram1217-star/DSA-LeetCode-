9f04c572b00469ab16aaee5879df8ed80ce57c10
Md Affan Akram <affanakram1217@gmail.com>
2026-09-07 22:32:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (8.94%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -3,21 +3,21 @@ public:
     int M=1e9+7;
     int dp[2001];
     vector<int> prev;
-    int solve(int n){
-        if(n==0){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int total=(2*solve(n-1))%M;
+    // int solve(int n){
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     int total=(2*solve(n-1))%M;
 
-        if(prev[n]!=0){
-            int duplicates=(solve(prev[n]-1));
-            total=(total-duplicates+M)%M;
-        }
-        return dp[n]=total;
-    }
+    //     if(prev[n]!=0){
+    //         int duplicates=(solve(prev[n]-1));
+    //         total=(total-duplicates+M)%M;
+    //     }
+    //     return dp[n]=total;
+    // }
     int distinctSubseqII(string s) {
         //Recursion + Memoization
         int n=s.length();
@@ -30,6 +30,19 @@ public:
             lastSeen[idx]=i;
         }
 
-        return (solve(n)-1+M)%M;
+        // return (solve(n)-1+M)%M;
+
+        //Bottom UP
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            int total=(2*dp[i-1])%M;
+            if(prev[i]!=0){
+                int duplicates=dp[prev[i]-1];
+                total=(total-duplicates+M)%M;
+            }
+            dp[i]=total;
+        }
+
+        return (dp[n]-1+M)%M;
     }
 };
