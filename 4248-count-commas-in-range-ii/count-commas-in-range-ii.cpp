aa78de48cf848daad71de5f124c428cd9020f2f5
Md Affan Akram <affanakram1217@gmail.com>
2026-09-09 20:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (84.80%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -2,21 +2,31 @@ class Solution {
 public:
     typedef long long ll;
     long long countCommas(long long n) {
-        //Approach 1
+        // //Approach 1
+        // ll result=0;
+        // ll lower=1000;
+        // ll comma=1;
+
+        // while(lower<=n){
+        //     ll upper=lower*1000-1;
+        //     if(upper>n){
+        //         upper=n;
+        //     }
+        //     ll CountNums=upper-lower+1;
+        //     result+=CountNums*comma;
+
+        //     lower*=1000;
+        //     comma+=1;
+        // }
+        // return result;
+
+        //Approach 2
         ll result=0;
-        ll lower=1000;
-        ll comma=1;
+        ll start=1000;
 
-        while(lower<=n){
-            ll upper=lower*1000-1;
-            if(upper>n){
-                upper=n;
-            }
-            ll CountNums=upper-lower+1;
-            result+=CountNums*comma;
-
-            lower*=1000;
-            comma+=1;
+        while(start<=n){
+            result+=(n-start+1);
+            start*=1000;
         }
         return result;
     }
