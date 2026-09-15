bf6e9cb7ef648c86799ec9c821d9d628d8731e05
Md Affan Akram <affanakram1217@gmail.com>
2026-09-15 20:15:08 +0530

    Time: 2319 ms (5.23%) | Memory: 208.2 MB (18.06%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int t[2001][2001];
-    bool isPalindrome(string& s, int i, int j){
+    // int t[2001][2001];
+    bool isPalindrome(string& s, int i, int j){  //used in both recursion and bottom up takes o(n) times
         while(i<=j){
             if(s[i]!=s[j]){
                 return false;
@@ -12,31 +12,55 @@ public:
         return true;
 
     }
-    int solve(string &s, int k, int i , int j){
-        if(i>=s.length() || j>=s.length()){
-            return 0;
-        }
-        if(t[i][j]!=-1){
-            return t[i][j];
-        }
-        if(isPalindrome(s,i,j)){
-            int take=1+solve(s,k,j+1,j+k);//non overlapping
-            int grow=solve(s,k,i,j+1);
-            int slide=solve(s,k,i+1,j+1);
+    // int solve(string &s, int k, int i , int j){
+    //     if(i>=s.length() || j>=s.length()){
+    //         return 0;
+    //     }
+    //     if(t[i][j]!=-1){
+    //         return t[i][j];
+    //     }
+    //     if(isPalindrome(s,i,j)){
+    //         int take=1+solve(s,k,j+1,j+k);//non overlapping
+    //         int grow=solve(s,k,i,j+1);
+    //         int slide=solve(s,k,i+1,j+1);
 
-            return t[i][j]=max({take,grow,slide});
-        }
+    //         return t[i][j]=max({take,grow,slide});
+    //     }
 
-        int grow=solve(s,k,i,j+1);
-        int slide=solve(s,k,i+1,j+1);
+    //     int grow=solve(s,k,i,j+1);
+    //     int slide=solve(s,k,i+1,j+1);
 
-        return t[i][j]=max(grow,slide);
-    }
+    //     return t[i][j]=max(grow,slide);
+    // }
     int maxPalindromes(string s, int k) {
+        // if(k==1){
+        //     return s.length();
+        // }
+        // memset(t,-1,sizeof(t));
+        // return solve(s,k,0,k-1);
+
+        int n=s.length();
         if(k==1){
-            return s.length();
+            return n;
         }
-        memset(t,-1,sizeof(t));
-        return solve(s,k,0,k-1);
+
+        vector<vector<int>> t(n+1,vector<int>(n+1));
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(isPalindrome(s,i,j)){
+                    int takeIt=1+(j+k<=n ? t[j+1][j+k]:0);
+                    int grow=t[i][j+1];
+                    int slide=t[i+1][j+1];
+
+                    t[i][j]=max({takeIt,grow,slide});
+                }
+                int grow=t[i][j+1];
+                int slide=t[i+1][j+1];
+                t[i][j]=max({t[i][j],grow,slide});
+            }
+        }
+
+        return t[0][k-1];
     }
 };
