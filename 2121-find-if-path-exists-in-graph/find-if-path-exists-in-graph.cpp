f8b26c5a90438a8e5e55205addcd372450107320
Md Affan Akram <affanakram1217@gmail.com>
2026-09-16 22:46:59 +0530

    Time: 247 ms (81.75%) | Memory: 281.8 MB (78.69%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool checkDFS(int src, int des,vector<vector<int>>& adj,vector<bool>& vis){
-        if(src==des){
-            return true;
-        }
-        vis[src]=true;
-        for(int v:adj[src]){
-            if(!vis[v]){
-                if(checkDFS(v,des,adj,vis)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+    // bool checkDFS(int src, int des,vector<vector<int>>& adj,vector<bool>& vis){
+    //     if(src==des){
+    //         return true;
+    //     }
+    //     vis[src]=true;
+    //     for(int v:adj[src]){
+    //         if(!vis[v]){
+    //             if(checkDFS(v,des,adj,vis)){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<vector<int>> adj(n);
         for(auto &edge:edges){
@@ -24,6 +24,28 @@ public:
             adj[v].push_back(u);
         }
         vector<bool> vis(n,false);
-        return checkDFS(source,destination,adj,vis);
+        // return checkDFS(source,destination,adj,vis);
+
+        //BFS 
+        queue<int> q;
+        q.push(source);
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+
+            if(node==destination){
+                return true;
+            }
+
+            for(int v: adj[node]){
+                if(!vis[v]){
+                    q.push(v);
+                    vis[v]=true;
+                }
+            }
+        }
+
+        return false;
     }
 };
