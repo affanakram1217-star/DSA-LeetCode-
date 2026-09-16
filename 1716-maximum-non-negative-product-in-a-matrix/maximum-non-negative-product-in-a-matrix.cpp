2ffed0725266e8c10f3fe4b4741f1e9964cc2735
Md Affan Akram <affanakram1217@gmail.com>
2026-09-16 20:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (69.26%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
     int M=1e9+7;
-    int m,n;
     typedef long long ll;
-    vector<vector<pair<ll,ll>>> t;
-    pair<ll,ll> solve(int i, int j, vector<vector<int>>& grid){
-        if(i==m-1 && j==n-1){
-            return {grid[i][j],grid[i][j]};
-        }
-
-        if(t[i][j]!=make_pair(LLONG_MIN,LLONG_MAX)){
-            return t[i][j];
-        }
-
-        ll maxVal=LLONG_MIN;
-        ll minVal=LLONG_MAX;
-
-        //down
-        if(i+1<m){
-            auto [downMax, downMin]=solve(i+1,j,grid);
-            maxVal=max({maxVal, grid[i][j]*downMin, grid[i][j]*downMax});
-            minVal=min({minVal, grid[i][j]*downMin,grid[i][j]*downMax});
-        }
-
-        //right
-        if(j+1<n){
-            auto [rightMax,rightMin]=solve(i,j+1,grid);
-            maxVal=max({maxVal,grid[i][j]*rightMin,grid[i][j]*rightMax});
-            minVal=min({minVal,grid[i][j]*rightMin,grid[i][j]*rightMax});
-        }
-
-        return t[i][j]={maxVal,minVal};
-    }
     int maxProductPath(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        t=vector<vector<pair<ll,ll>>>(m,vector<pair<ll,ll>>(n,{LLONG_MIN,LLONG_MAX}));
-        auto [maxProd,minProd]=solve(0,0,grid);
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<pair<ll,ll>>> t(m,vector<pair<ll,ll>>(n));
+
+        t[0][0]={grid[0][0],grid[0][0]};
+
+        //fill first row
+        for(int i=1;i<n;i++){
+            t[0][i].first=t[0][i-1].first*grid[0][i];//maxVal
+            t[0][i].second=t[0][i-1].second*grid[0][i];//minVal
+        }
+
+        //fill first col
+        for(int i=1;i<m;i++){
+            t[i][0].first=t[i-1][0].first*grid[i][0];//maxVal
+            t[i][0].second=t[i-1][0].second*grid[i][0];//minVal
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                ll upMax=t[i-1][j].first;
+                ll upMin=t[i-1][j].second;
+
+                ll lmax=t[i][j-1].first;
+                ll lmin=t[i][j-1].second;
+
+                t[i][j].first=max({upMax*grid[i][j],upMin*grid[i][j],lmax*grid[i][j],lmin*grid[i][j]});
+                t[i][j].second=min({upMax*grid[i][j],upMin*grid[i][j],lmax*grid[i][j],lmin*grid[i][j]});
+
+            }
+        }
+        auto [maxProd,minProd]=t[m-1][n-1];
         return maxProd<0?-1:maxProd%M;
     }
 };
